79bb90785b1d47cd53c54e597e341fda55e46b0a
Doug Knight <dougbknight@gmail.com>
2017-05-06 20:02:50 -0500

    implement calculation logic

--- app/models/calculation.rb
+++ app/models/calculation.rb
@@ -1,5 +1,9 @@
 class Calculation
-  REGEX = /\A([0-9\.]+)(?:\s*(\+|-|\/|\*)\s*([0-9\.]+))+\z/
+
+  NUMBER_REGEX = /\A\s*[0-9\.]+\s*\z/
+  OPERATION_REGEX = /\A\s*([0-9\.]+)\s*(\+|\-|\*|\/)\s*(.*)/
+  OPERATORS = %w(* / + -)
+  DEFAULT_OPTIONS = {places: 2}
 
   attr_accessor :input
 
@@ -7,15 +11,52 @@ class Calculation
     self.input = params[:input]
   end
 
-  def perform
-
+  def perform(options = {})
+    options = DEFAULT_OPTIONS.merge(options)
+    result = OPERATORS.reduce(parsed_input) do |input, operator|
+      if input.is_a? Array
+        process_operator(input, operator)
+      else
+        input
+      end
+    end
+    result.round(options[:places])
   end
 
   private
 
   def parsed_input
-    match = REGEX.match(input)
-    raise "Invalid input '#{input}'" unless match
-    match.to_a.drop(1)
+    @parsed_input ||= parse_input(input)
+  end
+
+  def parse_input(input)
+    # single number (end of input)
+    match = NUMBER_REGEX.match(input)
+    return [match[0].to_f] if match
+
+    # operation
+    match = OPERATION_REGEX.match(input)
+    raise "invalid input '#{input}'" unless match
+
+    [match[1].to_f, match[2]] + parse_input(match[3])
+  end
+
+  # calculate all operations of the specified type
+  # replacing the segements in the input with the result
+  def process_operator(input, operator)
+    while (index = input.index(operator)) do
+      segment = input[(index-1)..(index+1)]
+      result = segment[0].send(segment[1], segment[2])
+      return result if input.length == 3 # last operation
+
+      if index == 1
+        # first segment
+        input = [result] + input[(index+2)..-1]
+      else
+        # anything but the first segment
+        input = input[0..(index-2)] + [result] + input[(index+2)..-1]
+      end
+    end
+    input
   end
 end
--- spec/models/calculation_spec.rb
+++ spec/models/calculation_spec.rb
@@ -8,5 +8,26 @@ RSpec.describe Calculation do
         expect(calculation.perform).to eq 2
       end
     end
+
+    context 'given "1 + 1 + 1 + 1"' do
+      it 'returns 4' do
+        calculation = Calculation.new input: '1+1+1+1'
+        expect(calculation.perform).to eq 4
+      end
+    end
+
+    context 'given "5*3+1+6/2+9*100"' do
+      it 'returns 919' do
+        calculation = Calculation.new input: '5*3+1+6/2+9*100'
+        expect(calculation.perform).to eq 919
+      end
+    end
+
+    context 'given "5*3+1+6/85+9*100"' do
+      it 'returns 916.07' do
+        calculation = Calculation.new input: '5*3+1+6/85+9*100'
+        expect(calculation.perform).to eq 916.07
+      end
+    end
   end
 end
